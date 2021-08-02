d06152f3f596cf5baf8a0848f1c0ab235472c8be
Andrew Parsons <parsonsandrew1@gmail.com>
2021-08-02 10:24:10 -0400

    observer_pattern: attempt to fix problem with running Dagster pipelines, 11/?
    Signed-off-by: Andrew Parsons <parsonsandrew1@gmail.com>

--- QuickPotato/profiling/intrusive.py
+++ QuickPotato/profiling/intrusive.py
@@ -121,7 +121,7 @@ class PerformanceBreakpoint(Subject):
 
         if self.execution_wrapper:
             logging.info(f'Running self.execution_wrapper')
-            return self.execution_wrapper(update_wrapper(function, execute_function))
+            return self.execution_wrapper(update_wrapper(execute_function, function))
         return execute_function
 
     def attach(self, observer: Type[Interpreter]) -> None:
